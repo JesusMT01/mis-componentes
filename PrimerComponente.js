becceda87f8dcfec2e2a8f1efa6c5ebf93f91151
JesusMT01 <162145188+JesusMT01@users.noreply.github.com>
2024-09-17 21:45:47 -0600

    Add files via upload

---
+++ PrimerComponente.js
@@ -0,0 +1,27 @@
+//Importacion de dependency
+//import React from "react";
+
+
+//Declaracion de la funcion
+//export const MiPrimerComponente= ()=>{
+//    return(
+  //      <>
+   //     </>
+//    );
+//}
+import React from 'react';
+
+export const PrimerComponente = () => {
+    return (
+    <div>
+        <hr/>
+        <h2>Componente creador</h2>
+        <p>Este es mi primer Componente en React...!!!</p>
+        <ul>
+            <li>react</li>
+            <li>Anugular</li>
+            <li>Vue</li>
+        </ul>
+    </div>
+)   
+}
